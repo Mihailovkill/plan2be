f436a9aeefced1de6a17cc5e257f0ae5d2304b98
Mihailov <michhor8@gmail.com>
2023-09-01 10:10:30 +0200

    some changes

--- plan2be/src/app/api/auth/[...nextAuth]/route.ts
+++ plan2be/src/app/api/auth/[...nextAuth]/route.ts
@@ -1,9 +1,27 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-import NextAuth from "next-auth"
+import type { NextApiRequest, NextApiResponse } from "next";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { hash, compare } from "bcrypt";
+
+const prisma = new PrismaClient();
 
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-  // Do whatever you want here, before the request is passed down to `NextAuth`
-  return await NextAuth(req, res, {
-    ...
-  })
+  const providers = [
+    CredentialsProvider({
+      id: "domain-login",
+      name: "Domain Account",
+      async authorize(credentials, req) {
+        const user = {
+          /* add function to get user */
+        };
+        return user;
+      },
+      credentials: {
+        username: { label: "Username", type: "text ", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+    }),
+  ];
 }
---
+++ plan2be/src/pages/codeSnippets.tsx
@@ -0,0 +1,6 @@
+import Head from "next/head";
+import Header from "@/app/components/Header";
+
+export default function codeSnippets() {
+  return <h1>Hello there</h1>;
+}
